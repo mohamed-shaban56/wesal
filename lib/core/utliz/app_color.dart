b5410e6be5815e0378d1de8f80b437ff9aeedfe4
Mohamed Shaban <pmohamedshabaan@gmail.com>
2026-02-08 15:08:16 +0200

    feat: Implement initial application structure, core UI components, and feature views for home, splash, and search.

---
+++ lib/core/utliz/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const bottomNavigationBarInActiveColr = Color(0xff8E8C8F);
+  static const primaryColor = Color(0xffFF5722); // Orange
+  static const grey = Color(0xff9E9E9E);
+  static const lightGrey = Color(0xFF4B5563);
+  static const white = Colors.white;
+  static const lightOrange = Color(0xffFFF3E0);
+  static const darkOrange = Color(0xffE64A19);
+  static const purple = Color(0xff7B1FA2);
+}
---
+++ lib/core/utliz/app_text_style.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:wesal/core/utliz/app_color.dart';
+
+class AppTextStyle {
+  static TextStyle cairo13(BuildContext context) {
+    return const TextStyle(fontSize: 13, color: AppColor.white);
+  }
+
+  static TextStyle cairo16Bold(BuildContext context) {
+    return const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+
+      color: AppColor.white,
+    );
+  }
+
+  static TextStyle cairo12Bold(BuildContext context) {
+    return const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+
+      color: Colors.black, // Default color for categories
+    );
+  }
+
+  static TextStyle cairo18Bold(BuildContext context) {
+    return const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+
+      color: Colors.black,
+    );
+  }
+
+  static TextStyle cairo14(BuildContext context) {
+    return const TextStyle(fontSize: 14, color: AppColor.grey);
+  }
+
+  static TextStyle cairo14Bold(BuildContext context) {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    );
+  }
+
+  static TextStyle cairo14BoldPrimary(BuildContext context) {
+    return const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+
+      color: AppColor.primaryColor,
+    );
+  }
+}
