5028aacae48b9c8bfa0400d199f969b23f39f39e
Mohamed Shaban <pmohamedshabaan@gmail.com>
2026-02-08 08:38:21 +0200

    feat: Implement initial application structure and configure native splash screens for Android and iOS.

--- android/app/src/main/kotlin/com/example/wesal/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wesal/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.wesal
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs.BackgroundMode
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun getBackgroundMode(): BackgroundMode {
+        return BackgroundMode.transparent
+    }
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,12 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    controller.modalPresentationStyle = .overCurrentContext
+    controller.isViewOpaque = false
+    controller.view.backgroundColor = UIColor.clear
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
